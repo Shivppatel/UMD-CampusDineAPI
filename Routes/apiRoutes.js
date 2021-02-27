131ec20818496da24a521a54a86dbb208cfdcefd
Alex Leitch <aleitch1@umd.edu>
2021-02-27 12:29:15 -0500

    routes update to ES6/async-await

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,176 +1,213 @@
-const express = require("express");
+/* eslint-disable no-console */
+import express from 'express';
+import sequelize from 'sequelize';
+
+import db from '../models/initializeDB.js';
+
 const router = express.Router();
-const db = require("../models");
-const { QueryTypes } = require("sequelize");
 
-////////////////////////////////////
-///////Dining Hall Endpoints////////
-////////////////////////////////////
-router.get("/dining", (req, res) => {
-  db.Dining_Hall.findAll()
-    .then((halls) => res.send(halls))
-    .catch((error) => {
-      console.log(error);
+/// /////////////////////////////////
+/// ////Dining Hall Endpoints////////
+/// /////////////////////////////////
+router.get('/dining', async (req, res) => {
+  try {
+    const halls = await db.DiningHall.findAll();
+    res.json(halls);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get('/dining/:hall_id', async (req, res) => {
+  try {
+    const hall = await db.DiningHall.findAll({
+      where: {
+        hall_id: req.params.hall_id
+      }
     });
+    res.json(hall);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get("/dining/:hall_id", (req, res) => {
-  db.Dining_Hall.findAll({
-    where: {
-      hall_id: req.params.hall_id,
-    },
-  })
-    .then((hall) => res.send(hall))
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-router.post("/dining", (req, res) => {
-  db.Dining_Hall.create({
-    hall_id: req.body.hall_id,
-    hall_name: req.body.hall_name,
-    hall_location: req.body.hall_location,
-  })
-    .then((newDining) => res.send(newDining))
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-router.delete("/dining/:hall_id", (req, res) => {
-  db.Dining_Hall.destroy({
-    where: {
-      hall_id: req.params.hall_id,
-    },
-  }).then(() => res.send("Successfully Deleted"));
-});
-
-router.put("/dining", (req, res) => {
-  db.Dining_Hall.update(
-    {
+router.post('/dining', async (req, res) => {
+  try {
+    const newDining = await db.DiningHall.create({
+      hall_id: req.body.hall_id,
       hall_name: req.body.hall_name,
-      hall_location: req.body.hall_location,
-    },
-    {
+      hall_location: req.body.hall_location
+    });
+    res.json(newDining);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.delete('/dining/:hall_id', async (req, res) => {
+  try {
+    await db.DiningHall.destroy({
       where: {
-        hall_id: req.body.hall_id,
+        hall_id: req.params.hall_id
+      }
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.put('/dining', async (req, res) => {
+  try {
+    await db.DiningHall.update(
+      {
+        hall_name: req.body.hall_name,
+        hall_location: req.body.hall_location
       },
-    }
-  ).then(() => res.send("Successfully Updated"));
+      {
+        where: {
+          hall_id: req.body.hall_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-////////////////////////////////////
-///////////Meals Endpoints//////////
-////////////////////////////////////
-router.get("/meals", (req, res) => {
-  db.Meals.findAll()
-    .then((meals) => res.send(meals))
-    .catch((error) => {
-      console.log(error);
-    });
+/// /////////////////////////////////
+/// ////////Meals Endpoints//////////
+/// /////////////////////////////////
+router.get('/meals', async (req, res) => {
+  try {
+    const meals = await db.Meals.findAll();
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get("/meals/:meal_id", (req, res) => {
-  db.Meals.findAll({
-    where: {
-      meal_id: req.params.meal_id,
-    },
-  })
-    .then((meals) => res.send(meals))
-    .catch((error) => {
-      console.log(error);
-    });
-});
-router.put("/meals", (req, res) => {
-  db.Meals.update(
-    {
-      meal_name: req.body.meal_name,
-      meal_category: req.body.meal_category,
-    },
-    {
+router.get('/meals/:meal_id', async (req, res) => {
+  try {
+    const meals = await db.Meals.findAll({
       where: {
-        meal_id: req.body.meal_id,
-      },
-    }
-  ).then(() => res.send("Meal Successfully Updated"));
-});
-////////////////////////////////////
-///////////Macros Endpoints/////////
-////////////////////////////////////
-router.get("/macros", (req, res) => {
-  db.Macros.findAll()
-    .then((macros) => res.send(macros))
-    .catch((error) => {
-      console.log(error);
+        meal_id: req.params.meal_id
+      }
     });
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get("/macros/:meal_id", (req, res) => {
-  db.Macros.findAll({
-    where: {
-      meal_id: req.params.meal_id,
-    },
-  })
-    .then((meals) => res.send(meals))
-    .catch((error) => {
-      console.log(error);
-    });
+router.put('/meals', async (req, res) => {
+  try {
+    await db.Meals.update(
+      {
+        meal_name: req.body.meal_name,
+        meal_category: req.body.meal_category
+      },
+      {
+        where: {
+          meal_id: req.body.meal_id
+        }
+      }
+    );
+    res.send('Meal Successfully Updated');
+  } catch (err) {
+    console.error(err);
+  }
 });
-router.put("/macros", (req, res) => {
-  db.Macros.update(
-    {
-      meal_name: req.body.meal_name,
-      meal_category: req.body.meal_category,
-      calories: req.body.calories,
-      serving_size: req.body.serving_size,
-      cholesterol: req.body.cholesterol,
-      sodium: req.body.sodium,
-      carbs: req.body.carbs,
-      protein: req.body.protein,
-      fat: req.body.fat,
-    },
-    {
+
+/// /////////////////////////////////
+/// ////////Macros Endpoints/////////
+/// /////////////////////////////////
+router.get('/macros', async (req, res) => {
+  try {
+    const macros = await db.Macros.findAll();
+    res.send(macros);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get('/macros/:meal_id', async (req, res) => {
+  try {
+    const meals = await db.Macros.findAll({
       where: {
-        meal_id: req.body.meal_id,
+        meal_id: req.params.meal_id
+      }
+    });
+    res.json(meals);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.put('/macros', async (req, res) => {
+  try {
+    // N.B. - this is a good example of where to use code validation to confirm objects
+    await db.Macros.update(
+      {
+        meal_name: req.body.meal_name,
+        meal_category: req.body.meal_category,
+        calories: req.body.calories,
+        serving_size: req.body.serving_size,
+        cholesterol: req.body.cholesterol,
+        sodium: req.body.sodium,
+        carbs: req.body.carbs,
+        protein: req.body.protein,
+        fat: req.body.fat
       },
-    }
-  ).then(() => res.send("Successfully Updated"));
+      {
+        where: {
+          meal_id: req.body.meal_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-////////////////////////////////////
-///Dietary Restrictions Endpoints///
-////////////////////////////////////
-router.get("/restrictions", (req, res) => {
-  db.Dietary_Restrictions.findAll()
-    .then((restriction) => res.send(restriction))
-    .catch((error) => {
-      console.log(error);
+/// /////////////////////////////////
+/// Dietary Restrictions Endpoints///
+/// /////////////////////////////////
+router.get('/restrictions', async (req, res) => {
+  try {
+    const restrictions = await db.DietaryRestrictions.findAll();
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get('/restrictions/:restriction_id', async (req, res) => {
+  try {
+    const restrictions = await db.DietaryRestrictions.findAll({
+      where: {
+        restriction_id: req.params.restriction_id
+      }
     });
+    res.json(restrictions);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get("/restrictions/:restriction_id", (req, res) => {
-  db.Dietary_Restrictions.findAll({
-    where: {
-      restriction_id: req.params.restriction_id,
-    },
-  })
-    .then((restrictions) => res.send(restrictions))
-    .catch((error) => {
-      console.log(error);
-    });
+/// /////////////////////////////////
+/// //////Custom SQL Endpoint////////
+/// /////////////////////////////////
+router.get('/custom', async (req, res) => {
+  try {
+    const result = await db.sequelize
+      .query(req.body.query, { type: sequelize.QueryTypes.SELECT });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-////////////////////////////////////
-/////////Custom SQL Endpoint////////
-////////////////////////////////////
-router.get("/custom", (req, res) => {
-  db.sequelize
-    .query(req.body.query, { type: QueryTypes.SELECT })
-    .then((result) => res.send(result))
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-module.exports = router;
+export default router;
