dbb0d607ca9e91e33e1ad384ad39751e0b222434
Shiv Patel <shiv.p.patel@outlook.com>
2021-03-19 03:37:54 -0400

    Created model for Meals Locations table

---
+++ models/MealsLocations.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+    const Meals = sequelize.define(
+      'Meals_Locations',
+      {
+        hall_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        meal_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return Meals;
+  };
+  
