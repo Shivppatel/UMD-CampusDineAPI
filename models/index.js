fdcb40073b21dbc12a54b854e5de630777676f17
shivppatel <shiv.p.patel@outlook.com>
2021-03-23 12:08:58 -0400

    added MealsLocations model

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
+import MealsLocations from './MealsLocations.js';
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
-  Meals
+  Meals,
+  MealsLocations
 };
